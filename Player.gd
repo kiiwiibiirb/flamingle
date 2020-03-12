68a9915e3369f6c02a634b700255d718de2ec515
Sam <samcorbitt_@outlook.com>
2020-03-12 16:10:00 -0500

    sfgadsa
    hjgsadlksghlaserg

--- Player.gd
+++ Player.gd
@@ -13,21 +13,27 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("right"):
 		motion.x = min(motion.x+ACCELERATION, MAX_SPEED)
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("Run")
 	elif Input.is_action_pressed("left"):
 		motion.x = max(motion.x-ACCELERATION, -MAX_SPEED)
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("Run")
 	else:
+		$AnimatedSprite.play("Idle")
 		friction = true
 		motion.x = lerp(motion.x, 0, 0.2)
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("jump"):
+			$AnimatedSprite.play("Idle")
 			motion.y = JUMP_HEIGHT
 		if friction == true:
 			motion.x = lerp(motion.x, 0, 0.2)
 	else:
 		if friction == true:
 			motion.x = lerp(motion.x, 0, 0.05)
-		elif Input.is_action_just_pressed("jump"):
+		if Input.is_action_just_pressed("jump"):
 			motion.y = JUMP_HEIGHT
 	
 	if Input.is_action_just_pressed("quit"):
